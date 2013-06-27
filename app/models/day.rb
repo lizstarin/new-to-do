1975f7ca57a76811d26b08fc648eb069752fdee1
Nikias + James <nikias+james@appacademyio>
2013-06-27 11:13:56 -0400

    validations

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,4 +2,6 @@ class Day < ActiveRecord::Base
   attr_accessible :date
 
   has_many :tasks
+
+  validates :date, :uniqueness => true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ActiveRecord::Base
   attr_accessible :name, :due_date, :day_id, :completed
 
   belongs_to :day
+
+  # validates :name, :day_id, :completed, :presence => true
 end
